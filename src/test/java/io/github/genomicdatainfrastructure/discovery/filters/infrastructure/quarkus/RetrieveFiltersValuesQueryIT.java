59f1c805f3aecd7984c6285330937a34a645d981
jadz94 <jad.zeini94@gmail.com>
2024-11-11 16:29:48 +0100

    chore:  fix tests

---
+++ src/test/java/io/github/genomicdatainfrastructure/discovery/filters/infrastructure/quarkus/RetrieveFiltersValuesQueryIT.java
@@ -0,0 +1,8 @@
+package io.github.genomicdatainfrastructure.discovery.filters.infrastructure.quarkus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class RetrieveFiltersValuesQueryIT extends RetrieveFiltersValuesQueryTest {
+
+}

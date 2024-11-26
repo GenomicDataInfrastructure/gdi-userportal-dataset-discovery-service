3dbe99dde3b808c0ac335b1294651074d4961155
jadz94 <jad.zeini94@gmail.com>
2024-11-26 11:09:50 +0100

    chore: (ART-10688) update license

--- src/main/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/ckan/mapper/CkanMapper.java
+++ src/main/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/ckan/mapper/CkanMapper.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 PNED G.I.E.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package io.github.genomicdatainfrastructure.discovery.datasets.infrastructure.ckan.mapper;
 
 import io.github.genomicdatainfrastructure.discovery.model.RetrievedDataset;

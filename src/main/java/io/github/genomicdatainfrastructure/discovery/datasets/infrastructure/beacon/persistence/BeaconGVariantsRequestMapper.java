58530dedf620ebc927945d8a0d24ecdaf015620c
jadz94 <jad.zeini94@gmail.com>
2025-02-14 11:53:27 +0100

    fix: fix mapper

--- src/main/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/beacon/persistence/BeaconGVariantsRequestMapper.java
+++ src/main/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/beacon/persistence/BeaconGVariantsRequestMapper.java
@@ -6,17 +6,23 @@ package io.github.genomicdatainfrastructure.discovery.datasets.infrastructure.be
 
 import io.github.genomicdatainfrastructure.discovery.model.*;
 import io.github.genomicdatainfrastructure.discovery.remote.beacon.gvariants.model.*;
+import org.mapstruct.ap.shaded.freemarker.template.utility.CollectionUtils;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static io.github.genomicdatainfrastructure.discovery.model.GVariantsSearchResponse.*;
 import static io.github.genomicdatainfrastructure.discovery.remote.beacon.gvariants.model.BeaconRequestQuery.IncludeResultsetResponsesEnum.HIT;
 import static io.github.genomicdatainfrastructure.discovery.remote.beacon.gvariants.model.BeaconRequestQuery.RequestedGranularityEnum.RECORD;
+import static java.util.Optional.ofNullable;
 
 public class BeaconGVariantsRequestMapper {
 
     public static BeaconRequest map(GVariantSearchQuery query) {
+
         return BeaconRequest.builder()
                 .meta(new BeaconRequestMeta())
                 .query(BeaconRequestQuery.builder()
@@ -25,22 +31,31 @@ public class BeaconGVariantsRequestMapper {
                         .testMode(false)
                         .pagination(new BeaconRequestQueryPagination())
                         .filters(Collections.emptyList())
-                        .requestParameters(query.getParams())
+                        .requestParameters(query.getParams().entrySet().stream().filter(
+                                entry -> entry.getValue() != null).collect(Collectors.toMap(
+                                        Map.Entry::getKey, Map.Entry::getValue)))
                         .build())
                 .build();
 
     }
 
     public static List<GVariantsSearchResponse> map(BeaconResponse response) {
-
-        return response.getResponse().getResultSets().stream()
-                .map(BeaconGVariantsRequestMapper::mapResultSetToVariant)
-                .toList();
+        return ofNullable(response)
+                .map(BeaconResponse::getResponse)
+                .map(BeaconResponseContent::getResultSets)
+                .map(resultSets -> resultSets.stream()
+                        .map(BeaconGVariantsRequestMapper::mapResultSetToVariant)
+                        .toList())
+                .orElse(Collections.emptyList());
     }
 
     private static GVariantsSearchResponse mapResultSetToVariant(BeaconResultSet resultSet) {
+        if (resultSet == null) {
+            return null;
+        }
         GVariantsSearchResponse variant = new GVariantsSearchResponse();
         variant.beacon(resultSet.getBeaconId());
+        variant.dataset(resultSet.getId());
         resultSet.getResults().stream()
                 .flatMap(r -> r.getFrequencyInPopulations().stream())
                 .flatMap(fip -> fip.getFrequencies().stream())

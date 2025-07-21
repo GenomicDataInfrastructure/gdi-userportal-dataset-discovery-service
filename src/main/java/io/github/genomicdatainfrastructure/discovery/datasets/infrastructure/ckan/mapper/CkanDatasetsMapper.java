a1f544a7eda805171a7ebd750e184cfad424b5ba
Hans-Christian <hcvanderwerf@gmail.com>
2025-07-21 21:40:08 +0200

    feat(missing fields) Add missing fields to OpenAPI specs (#212)
    * feat(missing fields) Add missing fields to OpenAPI specs
    
    * Update src/main/openapi/ckan.yaml
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update src/main/openapi/ckan.yaml
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update src/main/openapi/ckan.yaml
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update src/main/openapi/ckan.yaml
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update src/main/openapi/ckan.yaml
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * fix(feat) updated OpenAPI specs with missing fields for Dataservice
    
    * fix UT
    
    * fix UT
    
    ---------
    
    Co-authored-by: Hans-christian <hanschristian.vanderwerf@health-ri.nl>
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- src/main/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/ckan/mapper/CkanDatasetsMapper.java
+++ src/main/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/ckan/mapper/CkanDatasetsMapper.java
@@ -4,10 +4,7 @@
 
 package io.github.genomicdatainfrastructure.discovery.datasets.infrastructure.ckan.mapper;
 
-import io.github.genomicdatainfrastructure.discovery.model.RetrievedDataset;
-import io.github.genomicdatainfrastructure.discovery.model.RetrievedDistribution;
-import io.github.genomicdatainfrastructure.discovery.model.SearchedDataset;
-import io.github.genomicdatainfrastructure.discovery.model.ValueLabel;
+import io.github.genomicdatainfrastructure.discovery.model.*;
 import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.*;
 import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapper;
@@ -46,6 +43,37 @@ public interface CkanDatasetsMapper {
     @Mapping(target = "languages", source = "language")
     @Mapping(target = "dcatType", source = "dcatType")
     @Mapping(target = "catalogue", ignore = true)
+    @Mapping(target = "qualifiedAttribution", source = "qualifiedAttribution")
+    @Mapping(target = "provenanceActivity", source = "provenanceActivity")
+    @Mapping(target = "qualityAnnotation", source = "qualityAnnotation")
+    @Mapping(target = "homepage", source = "homepage")
+    @Mapping(target = "uri", source = "uri")
+    @Mapping(target = "documentation", source = "documentation")
+    @Mapping(target = "frequency", source = "frequency")
+    @Mapping(target = "inSeries", source = "inSeries")
+    @Mapping(target = "isReferencedBy", source = "isReferencedBy")
+    @Mapping(target = "temporalCoverage", source = "temporalCoverage")
+    @Mapping(target = "retentionPeriod", source = "retentionPeriod")
+    @Mapping(target = "spatialCoverage", source = "spatialCoverage")
+    @Mapping(target = "accessRights", source = "accessRights")
+    @Mapping(target = "analytics", source = "analytics")
+    @Mapping(target = "applicableLegislation", source = "applicableLegislation")
+    @Mapping(target = "codeValues", source = "codeValues")
+    @Mapping(target = "codingSystem", source = "codingSystem")
+    @Mapping(target = "conformsTo", source = "conformsTo")
+    @Mapping(target = "legalBasis", source = "legalBasis")
+    @Mapping(target = "maxTypicalAge", source = "maxTypicalAge")
+    @Mapping(target = "minTypicalAge", source = "minTypicalAge")
+    @Mapping(target = "numberOfRecords", source = "numberOfRecords")
+    @Mapping(target = "numberOfUniqueIndividuals", source = "numberOfUniqueIndividuals")
+    @Mapping(target = "alternateIdentifier", source = "alternateIdentifier")
+    @Mapping(target = "personalData", source = "personalData")
+    @Mapping(target = "populationCoverage", source = "populationCoverage")
+    @Mapping(target = "purpose", source = "purpose")
+    @Mapping(target = "qualifiedRelation", source = "qualifiedRelation")
+    @Mapping(target = "temporalResolution", source = "temporalResolution")
+    @Mapping(target = "healthTheme", source = "healthTheme")
+    @Mapping(target = "versionNotes", source = "versionNotes")
     RetrievedDataset map(CkanPackage ckanPackage);
 
     @Mapping(target = "label", source = "displayName")
@@ -58,11 +86,54 @@ public interface CkanDatasetsMapper {
     @Mapping(target = "count", ignore = true)
     ValueLabel map(CkanTag ckanTag);
 
-    @Mapping(target = "title", source = "name")
+    @Mapping(target = "accessUrl", source = "accessUrl")
+    @Mapping(target = "downloadUrl", source = "downloadUrl")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "format", source = "format")
+    @Mapping(target = "languages", source = "language")
+    @Mapping(target = "retentionPeriod", source = "retentionPeriod")
+    @Mapping(target = "accessService", source = "accessServices")
     @Mapping(target = "createdAt", source = "issuedDate")
     @Mapping(target = "modifiedAt", source = "modifiedDate")
-    @Mapping(target = "languages", source = "language")
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "license", source = "license")
+    @Mapping(target = "mediaType", source = "mimetype")
+    @Mapping(target = "compressionFormat", source = "compressFormat")
+    @Mapping(target = "packagingFormat", source = "packageFormat")
+    @Mapping(target = "byteSize", source = "size")
+    @Mapping(target = "checksum", source = "hash")
+    @Mapping(target = "checksumAlgorithm", source = "hashAlgorithm")
+    @Mapping(target = "rights", source = "rights")
+    @Mapping(target = "availability", source = "availability")
+    @Mapping(target = "status", source = "status")
+    @Mapping(target = "temporalResolution", source = "temporalResolution")
+    @Mapping(target = "spatialResolutionInMeters", source = "spatialResolutionInMeters")
+    @Mapping(target = "conformsTo", source = "conformsTo")
+    @Mapping(target = "applicableLegislation", source = "applicableLegislation")
     RetrievedDistribution map(CkanResource ckanResource);
+
+    @Mapping(target = "id", source = "identifier")
+    @Mapping(target = "keywords", source = "keyword")
+    @Mapping(target = "languages", source = "language")
+    @Mapping(target = "license", source = "licence")
+    @Mapping(target = "accessRights", source = "accessRights")
+    @Mapping(target = "applicableLegislation", source = "applicableLegislation")
+    @Mapping(target = "conformsTo", source = "conformsTo")
+    @Mapping(target = "contact", source = "contact")
+    @Mapping(target = "creator", source = "creator")
+    @Mapping(target = "rights", source = "rights")
+    @Mapping(target = "description", source = "description")
+    @Mapping(target = "endpointDescription", source = "endpointDescription")
+    @Mapping(target = "endpointUrl", source = "endpointUrl")
+    @Mapping(target = "format", source = "format")
+    @Mapping(target = "landingPage", source = "landingPage")
+    @Mapping(target = "modified", source = "modified")
+    @Mapping(target = "publisher", source = "publisher")
+    @Mapping(target = "servesDataset", source = "servesDataset")
+    @Mapping(target = "theme", source = "theme")
+    @Mapping(target = "uri", source = "uri")
+    @Mapping(target = "title", source = "title")
+    AccessServiceInner map(CkanResourceAccessServicesInner source);
 
     default List<SearchedDataset> map(PackagesSearchResult result) {
         if (result == null || result.getResults() == null) {
@@ -83,6 +154,17 @@ public interface CkanDatasetsMapper {
     @Mapping(target = "catalogue", ignore = true)
     @Mapping(target = "recordsCount", ignore = true)
     SearchedDataset mapToSearchedDataset(CkanPackage ckanPackage);
+
+    @Mapping(target = "start", source = "start")
+    @Mapping(target = "end", source = "end")
+    TimeWindow map(CkanTimeWindow timeWindow);
+
+    @Mapping(target = "uri", source = "uri")
+    @Mapping(target = "text", source = "text")
+    @Mapping(target = "geom", source = "geom")
+    @Mapping(target = "bbox", source = "bbox")
+    @Mapping(target = "centroid", source = "centroid")
+    SpatialCoverage map(CkanSpatialCoverage spatialCoverage);
 
     default OffsetDateTime map(String date) {
         if (StringUtils.isBlank(date)) {

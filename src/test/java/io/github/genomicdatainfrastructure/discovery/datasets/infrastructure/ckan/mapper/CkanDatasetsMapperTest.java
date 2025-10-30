0120e0dae6bdd510be2bbb161a0127ce980cf640
Hans-Christian <hcvanderwerf@gmail.com>
2025-10-30 16:15:13 +0100

    Revert "fix: update CKAN schema for array-type fields" (#259)
    * Revert "fix: update CKAN schema for array-type fields"
    
    This reverts commit 1740c0d24d28009b547244c66d2c2ec4a2749ff6.
    
    * fix test
    
    ---------
    
    Co-authored-by: Hans-christian <hanschristian.vanderwerf@health-ri.nl>

--- src/test/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/ckan/mapper/CkanDatasetsMapperTest.java
+++ src/test/java/io/github/genomicdatainfrastructure/discovery/datasets/infrastructure/ckan/mapper/CkanDatasetsMapperTest.java
@@ -43,7 +43,6 @@ class CkanDatasetsMapperTest {
                     .contacts(List.of())
                     .creators(List.of())
                     .publishers(List.of())
-                    .keywords(List.of())
                     .datasetRelationships(List.of())
                     .dataDictionary(List.of())
                     .hdab(List.of())
@@ -372,7 +371,7 @@ class CkanDatasetsMapperTest {
                         getCkanValueLabel("Gender", "https://w3id.org/dpv/dpv-pd#Gender", 8),
                         getCkanValueLabel("Health Record",
                                 "https://w3id.org/dpv/dpv-pd#HealthRecord", 7)))
-                .populationCoverage(List.of("This example includes a very non-descript population"))
+                .populationCoverage("This example includes a very non-descript population")
                 .publisherNote(
                         "Health-RI is the Dutch health care initiative to build an integrated health data infrastructure for research and innovation.")
                 .publisherType(List.of(getCkanValueLabel("Undefined",
@@ -524,8 +523,8 @@ class CkanDatasetsMapperTest {
                 .build();
     }
 
-    private static @NotNull List<CkanValueLabel> getCkanTags() {
-        return List.of(getCkanValueLabel("key-tag", "key-tag"));
+    private static @NotNull List<String> getCkanTags() {
+        return List.of("key-tag");
     }
 
     private static @NotNull List<CkanValueLabel> getCkanValueLabels(String label, String value) {

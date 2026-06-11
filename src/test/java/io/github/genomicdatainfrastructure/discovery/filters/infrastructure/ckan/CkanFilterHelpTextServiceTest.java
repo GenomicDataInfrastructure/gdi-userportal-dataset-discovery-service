88afebfe9043a47e3bf25c2392a6d130a72d2c23
Hans-christian <hanschristian.vanderwerf@health-ri.nl>
2026-06-11 13:02:57 +0200

    Fix help text service unit test

--- src/test/java/io/github/genomicdatainfrastructure/discovery/filters/infrastructure/ckan/CkanFilterHelpTextServiceTest.java
+++ src/test/java/io/github/genomicdatainfrastructure/discovery/filters/infrastructure/ckan/CkanFilterHelpTextServiceTest.java
@@ -5,14 +5,14 @@
 package io.github.genomicdatainfrastructure.discovery.filters.infrastructure.ckan;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.genomicdatainfrastructure.discovery.model.Filter;
 import io.github.genomicdatainfrastructure.discovery.remote.ckan.api.CkanQueryApi;
 import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.CkanFilterHelpTextsResponse;
-import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.PackageSearchRequest;
-import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.PackagesSearchResponse;
-import jakarta.enterprise.inject.Vetoed;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -22,8 +22,17 @@ class CkanFilterHelpTextServiceTest {
 
     @Test
     void enrichUsesRequestedFilterKeysWhenCallingCkan() {
-        var ckanQueryApi = new StubCkanQueryApi();
+        var ckanQueryApi = mock(CkanQueryApi.class);
         var service = new CkanFilterHelpTextService(ckanQueryApi, new ObjectMapper());
+        var capturedKeys = new String[1];
+
+        when(ckanQueryApi.gdiFilterHelpTextsShow(anyString(), anyString())).thenAnswer(
+                invocation -> {
+                    capturedKeys[0] = invocation.getArgument(1, String.class);
+                    return CkanFilterHelpTextsResponse.builder()
+                            .result(Map.of())
+                            .build();
+                });
 
         var filters = List.of(
                 Filter.builder().key("title").build(),
@@ -32,39 +41,7 @@ class CkanFilterHelpTextServiceTest {
 
         service.enrich(filters, "en");
 
-        assertThat(ckanQueryApi.lastKeys)
+        assertThat(capturedKeys[0])
                 .isEqualTo("[\"title\",\"theme\"]");
-    }
-
-    @Vetoed
-    private static final class StubCkanQueryApi implements CkanQueryApi {
-
-        private String lastKeys;
-
-        @Override
-        public PackagesSearchResponse packageSearch(String acceptLanguage,
-                PackageSearchRequest packageSearchRequest) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public io.github.genomicdatainfrastructure.discovery.remote.ckan.model.CkanPackageShowResponse packageShow(
-                String id, String acceptLanguage) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public CkanFilterHelpTextsResponse gdiFilterHelpTextsShow(String acceptLanguage,
-                String keys) {
-            this.lastKeys = keys;
-            return CkanFilterHelpTextsResponse.builder()
-                    .result(Map.of())
-                    .build();
-        }
-
-        @Override
-        public String retrieveDatasetInFormat(String id, String format, String authorization) {
-            throw new UnsupportedOperationException();
-        }
     }
 }
